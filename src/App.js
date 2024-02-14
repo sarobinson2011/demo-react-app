9bd30841647a324e49d48e3d644c8d2544f2d188
sarobinson2001 <sarobinson2011@gmail.com>
2024-02-14 17:05:35 +0000

    Initial commit my-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
+import contract from './contracts/LockDrop.json';
+
+const lockdropAddress = "0xDB868ED97Ade086f2089e310202408346883F545";
+const abi = contract.abi;
+
+console.log(lockdropAddress);
+console.log(abi);
 
 function App() {
+
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  const checkWalletIsConnected = () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have Metamask installed");
+      return;
+    }
+    else {
+      console.log("Wallet exists... ready to go!")
+    }
+  }
+
+  const connectWalletHandler = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      alert("Please install Metamask");
+    }
+
+    try {
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      console.log("Success, account found! Address: ", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (err) {
+      console.log(err)
+    }
+
+  }
+
+  const connectWalletButton = () => {
+    return (
+      <button onClick={connectWalletHandler} className='cta-button connect-wallet-button'>
+        Connect Wallet
+      </button>
+    )
+  }
+
+  const depositHandler = () => {
+    // stuff
+  }
+
+  const depositButton = () => {
+    return (
+      <button onClick={depositHandler} className='cta-button deposit-button'>
+        Deposit
+      </button>
+    )
+  }
+
+  const withdrawHandler = () => {
+    // stuff
+  }
+
+  const withdrawButton = () => {
+    return (
+      <button onClick={withdrawHandler} className='cta-button withdraw-button'>
+        Withdraw
+      </button>
+    )
+  }
+
+  useEffect(() => {
+    checkWalletIsConnected();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-app'>
+      <h1>Web3 front-end sandbox</h1>
+      <div style={{ marginBottom: '10px' }}>
+        {connectWalletButton()}
+      </div>
+      <div style={{ marginBottom: '10px' }}>
+        {depositButton()}
+      </div>
+      <div style={{ marginBottom: '10px' }}>
+        {withdrawButton()}
+      </div>
     </div>
-  );
+  )
+
 }
 
 export default App;
+
+
+
+
+
